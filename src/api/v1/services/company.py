0a9f2b52ee83f02fa5cc65cdfe7650b76673e4d9
Sura <suri.murodova@gmail.com>
2024-10-14 21:13:27 +0300

    feat: implement account and company services

---
+++ src/api/v1/services/company.py
@@ -0,0 +1,23 @@
+from src.schemas.company import CompanyRequest, RegisterAccount
+from src.utils.service import BaseService
+from src.utils.unit_of_work import transaction_mode
+
+
+class Account(BaseService):
+    base_repository: str = 'account'
+
+    @transaction_mode
+    async def check_account(self, account: str):
+        return await self.uow.account.check_account(account)
+
+    @transaction_mode
+    async def create_account(self, account: RegisterAccount):
+        await self.uow.account.add_one(**account.model_dump())
+
+
+class Company(BaseService):
+    base_repository: str = 'company'
+
+    @transaction_mode
+    async def create_company(self, company: CompanyRequest):
+        await self.uow.company.add_one(**company.model_dump())
