49a86dec101d9676eab56caea034bea9f2fcad71
Sura <suri.murodova@gmail.com>
2024-10-14 22:06:07 +0300

    fix: add password hashing for create_company

--- src/api/v1/services/company.py
+++ src/api/v1/services/company.py
@@ -1,4 +1,5 @@
 from src.schemas.company import CompanyRequest, RegisterAccount
+from src.utils.security import hash_password
 from src.utils.service import BaseService
 from src.utils.unit_of_work import transaction_mode
 
@@ -20,4 +21,5 @@ class Company(BaseService):
 
     @transaction_mode
     async def create_company(self, company: CompanyRequest):
+        company.password = hash_password(company.password)
         await self.uow.company.add_one(**company.model_dump())
