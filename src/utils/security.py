9921126b62093250f6659a53c3c79945cf348426
Sura <suri.murodova@gmail.com>
2024-10-14 20:48:26 +0300

    feat: implement JWT encoding/decoding and password hashing utilities

---
+++ src/utils/security.py
@@ -0,0 +1,53 @@
+import bcrypt
+import jwt
+from fastapi import HTTPException, status
+
+from src.schemas.auth import AuthJwt
+from src.schemas.company import AdminJwtPayload
+
+auth_jwt_config = AuthJwt()
+
+
+def encode_jwt(
+        payload: AdminJwtPayload,
+        private_key: str = auth_jwt_config.private_key.read_text(),
+        algorithm: str = auth_jwt_config.algorithm,
+) -> str:
+    payload = payload.__dict__
+    encoded = jwt.encode(payload, private_key, algorithm=algorithm)
+    return encoded
+
+
+def decode_jwt(
+        token: str,
+        public_key: str = auth_jwt_config.public_key.read_text(),
+        algorithm: str = auth_jwt_config.algorithm,
+) -> str:
+    try:
+        decoded = jwt.decode(token, public_key, algorithm=[algorithm])
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    else:
+        return decoded
+
+
+def hash_password(
+        password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+        password: str,
+        hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
