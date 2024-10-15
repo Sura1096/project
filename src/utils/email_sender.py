3d058f49a15eeb44252254f318e2d253f059f299
Sura <suri.murodova@gmail.com>
2024-10-15 21:36:57 +0300

    feat: add functions to send email(tokens) to admins and users

--- src/utils/email_sender.py
+++ src/utils/email_sender.py
@@ -2,12 +2,11 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 import aiosmtplib
-from fastapi import APIRouter
+from fastapi import BackgroundTasks
 
 from src.core.config import email
 from src.schemas.company import MailBody
-
-company_router = APIRouter()
+from src.utils.security import encode_jwt
 
 
 async def send_email(msg: MailBody) -> dict:
@@ -26,3 +25,25 @@ async def send_email(msg: MailBody) -> dict:
         await server.sendmail(email.MAIL_FROM, msg.to, message.as_string())
     except aiosmtplib.SMTPRecipientsRefused as smtp_error:
         return {'status': f'Failed to send email. Invalid recipient: {msg.to}'}
+
+
+def send_token_to_admin(account: str, tasks: BackgroundTasks) -> None:
+    payload = {'sub': 'admin', 'email': account}
+    token = encode_jwt(payload)
+    data = {
+        'to': account,
+        'subject': 'Token',
+        'body': token,
+    }
+    tasks.add_task(send_email, MailBody(**data))
+
+
+def send_token_to_user(account: str, tasks: BackgroundTasks) -> None:
+    payload = {'sub': 'user', 'email': account}
+    token = encode_jwt(payload)
+    data = {
+        'to': account,
+        'subject': 'Token',
+        'body': token,
+    }
+    tasks.add_task(send_email, MailBody(**data))
