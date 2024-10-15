f477b35f1ab2d6fb486e281260c1a615541f7d49
Sura <suri.murodova@gmail.com>
2024-10-15 21:16:50 +0300

    feat: register user router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,6 +4,8 @@ __all__ = [
 from fastapi import APIRouter
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.user import user_router
 
 router = APIRouter()
-router.include_router(company_router, prefix='/v1', tags=['company'])
+router.include_router(company_router, prefix='/v1/company', tags=['company'])
+router.include_router(user_router, prefix='/v1/user', tags=['user'])
--- src/api/v1/routers/__init__.py
+++ src/api/v1/routers/__init__.py
@@ -1,5 +1,7 @@
 __all__ = [
     'company_router',
+    'user_router',
 ]
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.user import user_router
