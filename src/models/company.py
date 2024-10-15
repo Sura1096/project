9512ee7b858485d92d75eb7214a6c69b7ebc4b95
Sura <suri.murodova@gmail.com>
2024-10-15 21:25:06 +0300

    feat: add relationship with Company and to_pydantic_schema func

--- src/models/company.py
+++ src/models/company.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
+from src.schemas.company import AccountDB, CompanyDB
 
 
 class Account(Base):
@@ -10,6 +11,11 @@ class Account(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str]
     invite_token: Mapped[str]
+
+    company = relationship('Company', back_populates='account')
+
+    def to_pydantic_schema(self) -> AccountDB:
+        return AccountDB(**self.__dict__)
 
 
 class Company(Base):
