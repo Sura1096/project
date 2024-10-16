5274d28430e6117f91c9296abf25be54b1445c50
Sura <suri.murodova@gmail.com>
2024-10-16 15:20:10 +0300

    feat: implement authentication check functions

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,6 +1,6 @@
 import bcrypt
 import jwt
-from fastapi import HTTPException, status
+from fastapi import Header, HTTPException, status
 
 from src.schemas.auth import AuthJwt
 
@@ -49,3 +49,43 @@ def validate_password(
         password=password.encode(),
         hashed_password=hashed_password,
     )
+
+
+def validate_auth_user(
+        authorization: str = Header(...),
+) -> str:
+    if not authorization.startswith('Bearer '):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid authorization header format',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+
+    token = authorization[len('Bearer '):]  # Извлекаем сам токен
+    try:
+        # Декодируем JWT и получаем payload
+        payload = decode_jwt(token)
+        email = payload.get('email')
+        if email is None:
+            raise HTTPException(  # noqa: TRY301
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid token: email not found',
+                headers={'WWW-Authenticate': 'Bearer'},
+            )
+
+    except HTTPException:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
+    else:
+        return email
+
+
+def validate_email_from_token(email_from_token: str, curr_email: str) -> None:
+    if email_from_token != curr_email:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Email from token does not match with the provided email',
+        )
