ae090db183617239b78796c5cf474cc5d247d827
Sura <suri.murodova@gmail.com>
2024-10-16 11:26:48 +0300

    feat: add repository methods for updating user email and name

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from src.models.user import Secret, User
 from src.utils.repository import SqlAlchemyRepository
@@ -11,6 +11,25 @@ class UserRepository(SqlAlchemyRepository):
         query = select(self.model).where(self.model.email == email)
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
+
+    async def change_email(self, old_email: str, new_email: str) -> None:
+        query = (update(self.model)
+                 .where(self.model.email == old_email)
+                 .values(email=new_email)
+                 .execution_options(synchronize_session='fetch'))
+        await self.session.execute(query)
+
+    async def change_name(
+            self,
+            email: str,
+            new_first_name: str,
+            new_last_name: str,
+    ) -> None:
+        query = (update(self.model)
+                 .where(self.model.email == email)
+                 .values(first_name=new_first_name, last_name=new_last_name)
+                 .execution_options(synchronize_session='fetch'))
+        await self.session.execute(query)
 
 
 class SecretRepository(SqlAlchemyRepository):
