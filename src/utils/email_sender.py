9f96796d66f6099eda8baf7c1d949737d3e723b4
Sura <suri.murodova@gmail.com>
2024-10-18 11:13:10 +0300

    refactor: change functions for sending emails

--- src/utils/email_sender.py
+++ src/utils/email_sender.py
@@ -6,13 +6,14 @@ from fastapi import BackgroundTasks
 
 from src.core.config import email
 from src.schemas.company import MailBody
+from src.schemas.user import UpdateEmail, UserEmail
 from src.utils.security import decode_jwt, encode_jwt
 
 
 async def send_email(msg: MailBody) -> dict:
     message = MIMEMultipart('alternative')
     message['From'] = email.MAIL_FROM
-    message['To'] = msg.to
+    message['To'] = msg.to.email
     message['Subject'] = msg.subject
 
     part = MIMEText(msg.body, 'html')
@@ -21,14 +22,14 @@ async def send_email(msg: MailBody) -> dict:
     try:
         server = aiosmtplib.SMTP(email.MAIL_HOST, email.MAIL_PORT)
         await server.connect()
-        await server.login(email.MAIL_USERNAME, email.MAIL_PASSWORD)
-        await server.sendmail(email.MAIL_FROM, msg.to, message.as_string())
+        await server.login(msg.to.username, msg.to.password)
+        await server.sendmail(sender=email.MAIL_FROM, recipients=msg.to.email, message=message.as_string())
     except aiosmtplib.SMTPRecipientsRefused as smtp_error:
         return {'status': f'Failed to send email. Invalid recipient: {msg.to}'}
 
 
-def send_token_to_admin(account: str, tasks: BackgroundTasks) -> None:
-    payload = {'sub': 'admin', 'email': account}
+def send_token_to_admin(account: UserEmail, tasks: BackgroundTasks) -> None:
+    payload = {'sub': 'admin', 'email': account.email}
     token = encode_jwt(payload)
     data = {
         'to': account,
@@ -38,8 +39,8 @@ def send_token_to_admin(account: str, tasks: BackgroundTasks) -> None:
     tasks.add_task(send_email, MailBody(**data))
 
 
-def send_token_to_user(account: str, tasks: BackgroundTasks) -> None:
-    payload = {'sub': 'user', 'email': account}
+def send_token_to_user(account: UserEmail, tasks: BackgroundTasks) -> None:
+    payload = {'sub': 'user', 'email': account.email}
     token = encode_jwt(payload)
     data = {
         'to': account,
@@ -49,8 +50,9 @@ def send_token_to_user(account: str, tasks: BackgroundTasks) -> None:
     tasks.add_task(send_email, MailBody(**data))
 
 
-def send_token(invite_token: str, new_account: str, tasks: BackgroundTasks) -> None:
+def send_token(invite_token: str, new_account: UpdateEmail, tasks: BackgroundTasks) -> None:
     payload = decode_jwt(invite_token)
+    new_account = UserEmail(email=new_account.new_account, username=new_account.username, password=new_account.password)
     if payload.get('sub') == 'admin':
         tasks.add_task(send_token_to_admin, new_account, tasks)
     elif payload.get('sub') == 'user':
