eaf9906a53fa0f3e694059871be44b3d832909a7
Sura <suri.murodova@gmail.com>
2024-10-18 11:04:13 +0300

    fix: implement the correct endpoints

--- src/api/v1/routers/user.py
+++ src/api/v1/routers/user.py
@@ -26,28 +26,18 @@ async def create_user(
         account_service: Account = Depends(),
         user_service: User = Depends(),
         company_service: Company = Depends(),
-) -> None:
-    await check_if_account_exists(user.email, account_service)
-
-    company_info = await check_if_company_not_exists(user.company_name, company_service)
-    company_id = company_info.id
-    user_info = UserSaveDb(
-        company_id=company_id,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        email=user.email,
-    )
-    await user_service.create_user(user_info)
+) -> UserResponse:
+    return await user_service.create_user(user, account_service, company_service)
 
 
 @user_router.post('/sign-up')
 async def sign_up(
-        email: str,
+        email: UserEmail,
         background_tasks: BackgroundTasks,
         account_service: Account = Depends(),
-) -> None:
-    await check_if_account_exists(email, account_service)
-    background_tasks.add_task(send_token_to_user, email, background_tasks)
+        user_service: User = Depends(),
+) -> UserStatus:
+    return await user_service.sign_up(email, background_tasks, account_service)
 
 
 @user_router.post('/sign-up-complete')
@@ -56,11 +46,8 @@ async def sign_up_complete(
         account_service: Account = Depends(),
         secret_service: Secret = Depends(),
         user_service: User = Depends(),
-) -> None:
-    await check_if_account_exists(user.email, account_service)
-    await account_service.create_account(RegisterAccount(email=user.email, invite_token=user.token))
-    user_info = await check_if_user_not_exists(user.email, user_service)
-    await secret_service.add_secret(SecretSaveDb(user_id=user_info.id, password=user.password))
+) -> UserResponse:
+    return await user_service.sign_up_complete(user, account_service, secret_service)
 
 
 @user_router.post('/new-email')
@@ -68,10 +55,9 @@ async def send_token_to_another_email(
         email: UpdateEmail,
         background_tasks: BackgroundTasks,
         account_service: Account = Depends(),
-) -> None:
-    old_account_in_db = await check_if_account_not_exists(email.old_account, account_service)
-    await check_if_account_exists(email.new_account, account_service)
-    background_tasks.add_task(send_token, old_account_in_db.invite_token, email.new_account, background_tasks)
+        user_service: User = Depends(),
+) -> UserStatus:
+    return await user_service.send_token_to_another_email(email, background_tasks, account_service)
 
 
 @user_router.patch('/change-email')
@@ -79,10 +65,8 @@ async def change_email(
         email: UpdateEmailDb,
         account_service: Account = Depends(),
         user_service: User = Depends(),
-) -> None:
-    old_account_in_db = await check_if_account_not_exists(email.old_account, account_service)
-    await account_service.change_email(email)
-    await user_service.change_email(email.old_account, email.new_account)
+) -> UserResponse:
+    return await user_service.change_email(email, account_service)
 
 
 @user_router.patch('/change-name')
@@ -90,7 +74,5 @@ async def change_name(
         name: UpdateName,
         account_service: Account = Depends(),
         user_service: User = Depends(),
-) -> None:
-    old_account_in_db = await check_if_account_not_exists(name.account, account_service)
-    await user_service.change_name(name)
-
+) -> UserResponse:
+    return await user_service.change_name(name, account_service)
