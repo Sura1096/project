3ca98fa7c1dcd27e6e13baa62b53f703739152ea
Sura <suri.murodova@gmail.com>
2024-10-20 23:41:48 +0300

    feat: register department router.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,8 +4,10 @@ __all__ = [
 from fastapi import APIRouter
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.structure import department_router
 from src.api.v1.routers.user import user_router
 
 router = APIRouter()
 router.include_router(company_router, prefix='/v1/company', tags=['company'])
 router.include_router(user_router, prefix='/v1/user', tags=['user'])
+router.include_router(department_router, prefix='/v1/departments', tags=['org_structure'])
--- src/api/v1/routers/__init__.py
+++ src/api/v1/routers/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     'company_router',
+    'department_router',
     'user_router',
 ]
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.structure import department_router
 from src.api.v1.routers.user import user_router
