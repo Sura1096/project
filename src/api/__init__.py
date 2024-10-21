239b6d481870da796ecd7866898db8e1c1f33787
Sura <suri.murodova@gmail.com>
2024-10-21 12:26:25 +0300

    feat: register position and employee routers

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,10 +4,14 @@ __all__ = [
 from fastapi import APIRouter
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.employee import employee_router
+from src.api.v1.routers.position import position_router
 from src.api.v1.routers.structure import department_router
 from src.api.v1.routers.user import user_router
 
 router = APIRouter()
 router.include_router(company_router, prefix='/v1/company', tags=['company'])
 router.include_router(user_router, prefix='/v1/user', tags=['user'])
-router.include_router(department_router, prefix='/v1/departments', tags=['org_structure'])
+router.include_router(department_router, prefix='/v1/department', tags=['department'])
+router.include_router(position_router, prefix='/v1/position', tags=['position'])
+router.include_router(employee_router, prefix='/v1/employee', tags=['employee'])
--- src/api/v1/routers/__init__.py
+++ src/api/v1/routers/__init__.py
@@ -1,9 +1,13 @@
 __all__ = [
     'company_router',
     'department_router',
+    'employee_router',
+    'position_router',
     'user_router',
 ]
 
 from src.api.v1.routers.company import company_router
+from src.api.v1.routers.employee import employee_router
+from src.api.v1.routers.position import position_router
 from src.api.v1.routers.structure import department_router
 from src.api.v1.routers.user import user_router
